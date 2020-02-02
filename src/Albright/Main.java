3e93c2fdf930e4150923b5e096e803494e0e3d69
richyrich1 <46507776+richyrich1@users.noreply.github.com>
2020-02-01 18:51:00 -0700

    Update Main.java

--- src/Albright/Main.java
+++ src/Albright/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello from Connor.");
+        System.out.println("Hello Connor! My name is Richard.");
     }
 }
